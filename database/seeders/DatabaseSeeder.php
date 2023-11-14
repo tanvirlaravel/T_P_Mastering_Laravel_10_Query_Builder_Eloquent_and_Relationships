4dc89ad4de7861632352ddc3e4169656fa2da964
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-14 13:44:01 +0600

    3.21 done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         // Post::factory(10)->create();
-        $this->call(PostSeeder::class);
+        $this->call([
+            PostSeeder::class,
+        ]);
     }
 }
