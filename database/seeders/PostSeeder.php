d138f6cf4e802b466c1d213d22da50903a931ca9
Tanvir <34455693+tanvirlaravel@users.noreply.github.com>
2023-11-14 13:42:21 +0600

    3.20 seeding post table form json file

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class PostSeeder extends Seeder
 {
@@ -13,28 +14,19 @@ class PostSeeder extends Seeder
      */
     public function run(): void
     {
-        $posts = collect([
-            [
-                'title' => 'Post one',
-                'slug' =>'post-one',
-                'excerpt' => 'Excertp of post one',
-                'description' => 'Descriptoin of pst one',
-                'is_published' => true,
-                'min_to_read' => 2
-            ],
-            [
-                'title' => 'Post two',
-                'slug' =>'post-two',
-                'excerpt' => 'Excertp of post two',
-                'description' => 'Descriptoin of pst two',
-                'is_published' => true,
-                'min_to_read' => 34
-               ]
-        ]);
+        $json = File::get('database/json/posts.json');
+        $posts = collect(json_decode($json));
 
         $posts->each(function ($post) {
-            Post::create($post);
+            Post::create([
+                'title' => $post->title,
+                'slug' => $post->slug,
+                'excerpt' => $post->excerpt,
+                'description' => $post->description,
+                'is_published' => $post->is_published,
+                'min_to_read' => $post->min_to_read
+            ]);
         });
-       
+
     }
 }
